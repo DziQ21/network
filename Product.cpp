0464fffb95c45dc80cd9ee0dc619567ca54bca82
Michal Pogorzelec <michalpogo@student.agh.edu.pl>
2021-12-15 00:21:58 +0100

    cos tam napisalem

--- Product.cpp
+++ Product.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Product.hpp"
+
+Product::Product() {
+    if(!freed_IDs.empty()) {
+        ID_ = *freed_IDs.begin(); //przypisuje minimalny element
+        assigned_IDs.insert(ID_); // aktualizuje assigned
+    }
+    else if(!assigned_IDs.empty()) {
+        ID_ = *assigned_IDs.rbegin() + 1; //przypisuje maksymalny element + 1
+        assigned_IDs.insert(ID_);
+    }
+    else {
+        ID_ = 1;
+        assigned_IDs.insert(ID_);
+    }
+}
+
+// konstruktor przypisujący id, które pobralismy z pliku, wstawia id do assigned ids
+Product::Product(ElementID id): ID_(id) {
+    //TODO: chyba trzeba sprawdzić, czy wcześniej nie użyliśmy już tego id, ale nie jestem do końca pewien czy jest to
+    //konieczne, bo wczytujemy raczej na początku symulacji gdzie assigned_IDs jest puste
+    assigned_IDs.insert(ID_);
+}
+
+Product::Product(Product &&P) {
+    //TODO:
+}
+
+Product::~Product(){
+    assigned_IDs.erase(ID_);
+    freed_IDs.insert(ID_);
+}
---
+++ types.hpp
@@ -0,0 +1,12 @@
+//
+// Created by micha on 14.12.2021.
+//
+
+#ifndef SIEC_TYPES_HPP
+#define SIEC_TYPES_HPP
+
+using ElementID = int;
+
+
+
+#endif //SIEC_TYPES_HPP
