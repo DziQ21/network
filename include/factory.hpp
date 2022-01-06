0f3ed06a93f7e9bfa726bc893675faa768e0ab59
Katarzyna Luszczewska <luszczewskakasia>
2022-01-06 14:38:16 +0100

    poprawa

--- include/factory.hpp
+++ include/factory.hpp
@@ -6,7 +6,7 @@
 #define NETWORK_FACTORY_HPP
 #include "storage_types.hpp"
 #include "nodes.hpp"
-#include <list>
+#include <vector>
 
 
 template <class Node>
@@ -16,32 +16,45 @@ public:
     void add(Node&&(node));
     void remove_by_id(ElementID id);
 
-    using container_t = typename std::list<Node>;
+    using container_t = typename std::vector<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
-    NodeCollection<Node>::iterator begin() {return find_by_id(ElementID id);};
-    NodeCollection<Node>::const_iterator cbegin() find_by_id(ElementID id);
+    NodeCollection<Node>::iterator find_by_id(ElementID id);
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id) const;
 
 
 };
 
 class Factory {
 private:
-    void NodeCollection<Ramp>::const_iterator remove_receiver();
+    void remove_receiver(NodeCollection<Ramp>::const_iterator& collection, ElementID id);
 public:
     void add_ramp(Ramp&&);
     void remove_ramp(ElementID id);
-    NodeCollection<Ramp>::iterator begin() find_ramp_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator cbegin() find_ramp_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator ramp_cbegin();
-    NodeCollection<Ramp>::const_iterator  ramp_cend();
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const;
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() const;
+    NodeCollection<Ramp>::const_iterator  ramp_cend() const;
 
-    bool is_considered(void);
+    void add_worker(Worker&&);
+    void remove_worker(ElementID id);
+    NodeCollection<Ramp>::iterator find_worker_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator find_worker_by_id(ElementID id) const;
+    NodeCollection<Ramp>::const_iterator worker_cbegin() const;
+    NodeCollection<Ramp>::const_iterator  worker_cend() const;
+
+    void add_storehouse(Storehouse&&);
+    void remove_storehouse(ElementID id);
+    NodeCollection<Ramp>::iterator find_storehouse_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator find_storehouse_by_id(ElementID id) const;
+    NodeCollection<Ramp>::const_iterator storehouse_cbegin() const;
+    NodeCollection<Ramp>::const_iterator  storehouse_cend() const;
+
+    bool is_considered();
     void do_deliveries(Time);
-    void do_package_passing(void);
+    void do_package_passing();
     void do_work(Time);
 
 };
 
-// czy te klasy Ramps, Workes i Storehouses mam tu wrzucić puste?
 #endif //NETWORK_FACTORY_HPP
