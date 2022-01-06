976cc772ea57e388d3b49e18d6e21f2d7fbf0721
Pawel Radzik <pawel.radzik321@gmail.com>
2022-01-06 10:48:57 +0100

    better name for rng

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -5,7 +5,7 @@
 #ifndef NETWORK_HELPERS_HPP
 #define NETWORK_HELPERS_HPP
 #include <stdlib.h>
-double get_random_number(){
+double probability_generator(){
     return (double)rand()/(double)RAND_MAX;
 }
 #endif //NETWORK_HELPERS_HPP
