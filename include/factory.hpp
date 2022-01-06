af5449044bcd92e12cf6dc495a0abec6980663d4
Katarzyna Luszczewska <luszczewskakasia>
2022-01-06 14:58:08 +0100

    'consty'

--- include/factory.hpp
+++ include/factory.hpp
@@ -21,6 +21,8 @@ public:
     using const_iterator = typename container_t::const_iterator;
     NodeCollection<Node>::iterator find_by_id(ElementID id);
     NodeCollection<Node>::const_iterator find_by_id(ElementID id) const;
+    NodeCollection<Node>::const_iterator cbegin() const;
+    NodeCollection<Node>::const_iterator cend() const;
 
 
 };
@@ -28,27 +30,30 @@ public:
 class Factory {
 private:
     void remove_receiver(NodeCollection<Ramp>::const_iterator& collection, ElementID id);
+    NodeCollection<Ramp> ramp_container;
+    NodeCollection<Worker> worker_container;
+    NodeCollection<Storehouse> storehouse_container;
 public:
     void add_ramp(Ramp&&);
     void remove_ramp(ElementID id);
-    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id);
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return ramp_container.find_by_id(id);};
     NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const;
     NodeCollection<Ramp>::const_iterator ramp_cbegin() const;
     NodeCollection<Ramp>::const_iterator  ramp_cend() const;
 
     void add_worker(Worker&&);
     void remove_worker(ElementID id);
-    NodeCollection<Ramp>::iterator find_worker_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator find_worker_by_id(ElementID id) const;
-    NodeCollection<Ramp>::const_iterator worker_cbegin() const;
-    NodeCollection<Ramp>::const_iterator  worker_cend() const;
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return worker_container.find_by_id(id);};
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const;
+    NodeCollection<Worker>::const_iterator worker_cbegin() const;
+    NodeCollection<Worker>::const_iterator  worker_cend() const;
 
     void add_storehouse(Storehouse&&);
     void remove_storehouse(ElementID id);
-    NodeCollection<Ramp>::iterator find_storehouse_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator find_storehouse_by_id(ElementID id) const;
-    NodeCollection<Ramp>::const_iterator storehouse_cbegin() const;
-    NodeCollection<Ramp>::const_iterator  storehouse_cend() const;
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return storehouse_container.find_by_id(id);};
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const;
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const;
+    NodeCollection<Storehouse>::const_iterator  storehouse_cend() const;
 
     bool is_considered();
     void do_deliveries(Time);
